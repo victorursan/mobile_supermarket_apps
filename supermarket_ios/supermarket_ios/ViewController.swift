1355a0056faba71569b053eebe5f0daaa3e0338b
victorursan <victor.ursan@gmail.com>
2016-10-24 00:09:29 +0300

    added ios base structure

---
+++ supermarket_ios/supermarket_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  supermarket_ios
+//
+//  Created by Victor Ursan on 10/24/16.
+//  Copyright © 2016 Victor Ursan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
